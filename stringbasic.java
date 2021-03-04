9c4992b15eb6348b60ff86a131a59f6a13a43f61
kimjinoh <wlsdh1110@gmail.com>
2021-03-05 02:51:57 +0900

    210305

---
+++ stringbasic.java
@@ -0,0 +1,23 @@
+
+public class stringbasic {
+
+	public static void main(String[] args) {
+		class Solution {
+			public boolean solution(String s) {
+				boolean answer = false;
+				if (s.length() == 4 || s.length() == 6) {
+					for (int i = 0; i < s.length(); i++) {
+						if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+							answer = true;
+						} else {
+							answer = false;
+							break;
+						}
+					}
+				}
+				return answer;
+			}
+		}
+	}
+
+}
