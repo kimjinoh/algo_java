f99d154dc1c4cda9b05a8381e469cf3e786b529a
kimjinoh <wlsdh1110@gmail.com>
2021-03-07 00:57:55 +0900

    210307

--- weirdString.java
+++ weirdString.java
@@ -1,47 +1,48 @@
 import java.util.*;
+
 public class weirdString {
+//	public String solution(String s) {
+//        String answer = "";
+//		ArrayList<Character> arr = new ArrayList<Character>();
+//		for (int i = 0; i < s.length(); i++) {
+//			arr.add(s.charAt(i));
+//		}
+//		String[] ans = new String[arr.size()];
+//		int k = 0;
+//		for (int i = 0; i < arr.size(); i++) {
+//			if (arr.get(i) == ' ') {
+//				k = 0;
+//				ans[i] = "";
+//				continue;
+//			}
+//			if (k % 2 == 0)
+//				ans[i] = Character.toString(arr.get(i)).toUpperCase();
+//			else
+//				ans[i] = Character.toString(arr.get(i)).toLowerCase();
+//			k++;
+//		}
+//		for (int i = 0; i < ans.length; i++) {
+//			if (arr.get(i) == ' ')
+//				answer += " ";
+//			answer += ans[i];
+//		}
+//        return answer;
+//    }
 	public String solution(String s) {
-        String answer = "";
-        ArrayList<String> abc = new ArrayList<String>();
-        int sss = 0;
-        for(int i = 0; i < s.length(); i++){
-            String str ="";
-            if(s.charAt(i) == ' '){
-                for(int j = sss; j < i; j++){
-                    str += s.charAt(j);
-                }
-                sss = i;
-                abc.add(str);
-            }
-        }
-        int size = 0;
-        for(int i = 0 ; i < abc.get(i).length(); i++)
-            size += abc.get(i).length()
-        for(int j = 0; j < abc.size(); j++){
-            String word = "";
-            for(int i = 0 ; i < abc.get(j).length(); i++){
-                
-            }
-        }
-        for(int i = 0; i < abc.get(i).length(); i++){
-            if(i%2 == 0)
-                ans[i] = Character.toString(arr.get(i)).toUpperCase();
-            else
-                ans[i] = Character.toString(arr.get(i)).toLowerCase();
-            answer += ans[i];
-        }
-        
-        return answer;
-    }
+		String answer = "";
+		int cnt = 0;
+		String[] arr = s.split("");
+		for(String ss : arr) {
+			cnt = ss.contains(" ")?0:cnt+1;
+			answer += cnt%2==0?ss.toLowerCase():ss.toUpperCase();
+		}
+		return answer;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "try hello world";
-        ArrayList<Character> arr =new ArrayList<Character>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) != ' ')
-                arr.add(s.charAt(i));
-        }
-        System.out.println(arr);
+		weirdString sol = new weirdString();
+		System.out.println(sol.solution(s));
 	}
 
 }
